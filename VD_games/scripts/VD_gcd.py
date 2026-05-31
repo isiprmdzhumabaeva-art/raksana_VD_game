6073076c004834c49c4112c3c18386639757ebf9
isiprmdzhumabaeva-art <isip_i.dzhumabaeva@mpt.ru>
2026-05-31 23:37:40 +0300

    Add VD-gcd game

---
+++ VD_games/scripts/VD_gcd.py
@@ -0,0 +1,24 @@
+import random
+from math import gcd
+
+from VD_games.engine import run_game
+
+DESCRIPTION = "Find the greatest common divisor of given numbers."
+
+
+def generate_round():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+
+    answer = gcd(num1, num2)
+
+    question = f"{num1} {num2}"
+    return question, answer
+
+
+def main():
+    run_game(generate_round, DESCRIPTION)
+
+
+if __name__ == "__main__":
+    main()
